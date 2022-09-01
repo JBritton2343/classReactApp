d6d01bff61329d109b0d927c725d2f3c7c5a2640
James Britton <jbritton@stu.jsu.edu>
2022-08-31 19:11:21 -0500

    Made component folders and formatted for spacing

---
+++ src/Components/Jumbotron/Jumbotron.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const jumbotron = () => {
+  const Jumbotron = (props) => {
+    return (
+      <div className="jumbotron m-5">
+        <h1 className="display-4">{props.title}</h1>
+        <p className="lead">{props.jumboText}</p>
+        <a
+          className="btn btn-primary btn-lg"
+          href={props.buttonURL}
+          role="button"
+        >
+          {props.buttonLabel}
+        </a>
+      </div>
+    );
+  };
+
+  Jumbotron.propTypes = {
+    //proptypes here
+    title: PropTypes.string,
+    description: PropTypes.string,
+    buttonLabel: PropTypes.string,
+    buttonURL: PropTypes.string,
+  };
+};
+
+export default jumbotron;
