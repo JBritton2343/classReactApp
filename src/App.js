f4f8ad139170358f16d425f853ecaab7acf894c6
James Britton <jbritton@stu.jsu.edu>
2022-08-31 20:22:26 -0500

    added wrapper div on app.js and return and format

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import React from "react";
-import ReactDOM from "react-dom";
+
 import Jumbotron from "./Components/Jumbotron/Jumbotron";
 import Card from "./Components/Card/Card";
 import NavBar from "./Components/NavBar/NavBar";
 
-const App=(props)=>{
-    <NavBar />;
-    <Jumbotron />;
-    <div className="container-fluid d-flex">
+function App() {
+  return (
+    <div>
+      <NavBar />;
+      <Jumbotron />;
+      <div className="container-fluid d-flex">
         <Card cardText="ultricies mi quis hendrerit" />;
         <Card cardText="blandit aliquam etiam erat velit" />;
         <Card cardText="sem viverra aliquet eget sit amet tellus" />;
         <Card cardText="morbi blandit cursus risus at ultrices" />;
+      </div>
     </div>
-
+  );
 }
-
-
-
 
 export default App;
