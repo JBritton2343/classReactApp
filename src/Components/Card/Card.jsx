eed33bc37fff4886d9a307c874fd03806fe26afe
JBritton2343 <jbritton854@gmail.com>
2022-09-02 18:30:27 -0500

    added style objects to card

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -1,14 +1,24 @@
 import React from "react";
 
 
-const Card = (props)=>{
-    <div className="card" style={"width: 18rem"}>;
-  <img src={require("../images/greybox.png")} className="card-img-top" alt="greyBox" sytle={"max-height: 325, max-width: 500"} />;
-  <div className="card-body">;
-    <h5 className="card-title">Card title</h5>;
-    <p className="card-text">{props.cardText}</p>
-    <a href="#" className="btn btn-primary">Go somewhere</a>
-  </div>
-</div>
+const Card = (props) => {
+  const cardStyleObj={
+    width: "18rem"
+  }
+  const imgStyleObj={
+    height: "325px",
+    width: "500px"
+
+  }
+  
+  return(
+  <div className="card" style={cardStyleObj}>;
+    <img src={require("../images/greybox.png")} className="card-img-top" alt="greyBox" sytle={imgStyleObj} />;
+    <div className="card-body">;
+      <h5 className="card-title bg-success text-white">Card title</h5>;
+      <p className="card-text">{props.cardText}</p>
+      <a href="#" className="btn btn-primary">Go somewhere</a>
+    </div>
+  </div>);
 }
 export default Card;
